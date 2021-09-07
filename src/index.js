71e481f78f0360969b21b8a3d4a3bb0e0d951d73
Enio N. de Souza <enionsouza@gmail.com>
2021-09-07 14:22:53 -0400

    feat: Add different API query types

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 import './style.css';
 
 const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
-
-// event listeners
-searchBtn.addEventListener('click', getMealList);
-mealList.addEventListener('click', getMealRecipe);
-recipeCloseBtn.addEventListener('click', () => {
-  mealDetailsContent.parentElement.classList.remove('showRecipe');
-});
-
+const queryOptions = {
+  'Ingredient': 'https://www.themealdb.com/api/json/v1/1/filter.php?i=',
+  'Meal name': 'https://www.themealdb.com/api/json/v1/1/search.php?s=',
+  'Category': 'https://www.themealdb.com/api/json/v1/1/filter.php?c=',
+  'Area': 'https://www.themealdb.com/api/json/v1/1/filter.php?a=',
+  'First letter': 'https://www.themealdb.com/api/json/v1/1/search.php?f=',
+  'Id': 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=',
+};
+let selectedQuery = 'Ingredients';
 
 // get meal list that matches with the ingredients
-function getMealList() {
+const getMealList = (e) => {
+  e.preventDefault();
   let searchInputTxt = document.getElementById('search-input').value.trim();
-  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+  let url = `${queryOptions[selectedQuery]}${searchInputTxt}`;
+  console.log(url);
+  fetch(url)
     .then(response => response.json())
     .then(data => {
       let html = "";
@@ -76,4 +86,26 @@ function mealRecipeModal(meal) {
     `;
   mealDetailsContent.innerHTML = html;
   mealDetailsContent.parentElement.classList.add('showRecipe');
+}
+
+
+// event listeners
+searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click', getMealRecipe);
+// recipeCloseBtn.addEventListener('click', () => {
+//   mealDetailsContent.parentElement.classList.remove('showRecipe');
+// });
+
+const ulQueries = document.querySelector('.dropdown-menu');
+Object.keys(queryOptions).forEach((queryOption) => {
+  ulQueries.innerHTML += `<li><a class="dropdown-item" href="#">${queryOption}</a></li>`
+});
+
+const queryTypes = document.querySelectorAll('.dropdown-item');
+// eslint-disable-next-line no-plusplus
+for (let i = 0; i < queryTypes.length; i++) {
+  // eslint-disable-next-line no-loop-func
+  queryTypes[i].addEventListener('click', () => {
+    selectedQuery = queryTypes[i].textContent;
+  });
 }
