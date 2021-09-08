ffe0a0c119d66f33741cf3a2a4b32f6869fe6719
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-09-08 20:52:29 +0100

    feat: Add postLike and postComment

--- src/involvement.js
+++ src/involvement.js
@@ -1,1 +1,25 @@
 const appId = 'yl0Oe9JiB7wA53s9o4SG';
+
+export const postLike = (idMeal) => {
+  fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes`, {
+    method: 'POST',
+    body: JSON.stringify({ item_id: idMeal }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  // .then((response) => response.json())
+  // .then((json) => console.log(json));
+};
+
+export const postComment = (idMeal, username, comment) => {
+  fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/comments`, {
+    method: 'POST',
+    body: JSON.stringify({ item_id: idMeal, username, comment }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+};
